0b2561abad124568f22a73f7db1064d06292520e
florian1105 <florian110599@gmail.com>
2018-10-08 11:35:55 +0200

    méthode racine

--- src/programme/racineCarre.java
+++ src/programme/racineCarre.java
@@ -1,5 +1,27 @@
 package programme;
 
 public class racineCarre {
-
+	
+	int tab[] ;
+	int a,b ; 
+	public racineCarre(int a , int b) {
+		
+		if ( a<b) {
+		this.a=a;
+		this.b=b; }
+		else System.exit(-1);
+	}
+	
+	public int[] racineCarre(){
+		int j = 0 ;
+		
+		
+		for(int i=a ; i<=b; i++) {
+			tab[j]=(int) Math.sqrt(i);
+			j++;
+		}
+		return tab ; 
+	}
+	
+	
 }
